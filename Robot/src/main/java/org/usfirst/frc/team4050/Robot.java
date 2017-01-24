c54b71a379a9f1163bbe2c9639261d52e9f91dfa
Carson <cds17257@gmail.com>
2017-01-23 20:23:09 -0500

    Added main project files

---
+++ Robot/src/main/java/org/usfirst/frc/team4050/Robot.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team4050;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Created by Carson on 1/23/2017.
+ */
+public class Robot extends IterativeRobot {
+
+    @Override
+    public void robotInit() {
+        super.robotInit();
+    }
+
+    @Override
+    public void disabledInit() {
+        super.disabledInit();
+    }
+
+    @Override
+    public void autonomousInit() {
+        super.autonomousInit();
+    }
+
+    @Override
+    public void teleopInit() {
+        super.teleopInit();
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        super.disabledPeriodic();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        super.autonomousPeriodic();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        super.teleopPeriodic();
+    }
+}
